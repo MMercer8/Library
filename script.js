f97ee85a7e3f28eadd102f9a2f5d4b7424dda47b
Michael Mercer <michaelmercer89@gmail.com>
2023-03-14 12:54:52 -0400

    adjusted

--- script.js
+++ script.js
@@ -17,14 +17,12 @@ let read = 0;
 let unread = 0;
 
 closeBtn.onclick = function() {
-// console.log("should be closing");
     modal.style.display = "none";
 }
 
 addBookButton.onclick = function() {
     modal.style.display = "block";
     clearInputFields();
-    // console.log("add");
 }
 
 window.onclick = function (event) {
@@ -40,15 +38,12 @@ function Book(title, author, pages, haveRead, idNum) {
     this.pages = pages,
     this.haveRead = haveRead,
     this.idNum = idNum;
-    
 }
 
 function addBookToLibrary() {
    let book = new Book(title.value, author.value, pages.value, haveRead.checked, bookCount);
     myLibrary.push(book);
     modal.style.display = "none";
-
-    updateBookCounterNew(haveRead.checked);
     drawGrid();
 }
 
@@ -95,9 +90,11 @@ function addBookToList(title, author, pages, haveRead, i) {
     bookGrid.appendChild(card);
     deleteButton.addEventListener('click', (e) => {
         deleteBook(e);
+        ubc();
     });
    cardHaveRead.addEventListener('change', (e) => {
         changeReadStatus(e);
+        ubc();
    });
 }
 
@@ -105,92 +102,62 @@ function deleteBook(e) {
     //remove item from DOM
     e.target.parentElement.remove();
     myLibrary.splice(e.idNum, 1);
+    ubc();
     drawGrid();
 }
 
-// currently getting the event from the check change to affect the
-// css and more importantly the OBJECT in the myLibrary array
 function addEListener(checkbox) {
     checkbox.addEventListener('change', (e) => {
         console.log(e.target);
     });
 }
 
-
 function drawGrid() {
     bookGrid.innerHTML = "";
-    bookCount = 0;
-    read = 0;
-    unread = 0;
     for (let i = 0; i < myLibrary.length; i++) {
         addBookToList(myLibrary[i].title, myLibrary[i].author, myLibrary[i].pages, myLibrary[i].haveRead, i);
         myLibrary[i].idNum = i;
-        bookCount++;
-        bookCountDisplay.innerText = bookCount;
-        updateBookCounter(myLibrary[i].haveRead);
     }  
+    ubc();
 }
-
 
 function changeReadStatus(e) {
     if(e.target.checked === true) {
         e.target.parentElement.parentElement.style.backgroundColor = 'red';
-        updateBookCounter(true);
+        let o = parseInt(e.target.parentElement.parentElement.getAttribute("data-id"));
+        myLibrary[o].haveRead = true;
+        ubc();
     } else {
         e.target.parentElement.parentElement.style.backgroundColor = 'blue';
-        updateBookCounter(false);
+        let o = parseInt(e.target.parentElement.parentElement.getAttribute("data-id"));
+        myLibrary[o].haveRead = false;      
+        ubc();
     }
     
 }
 
 
-function updateBookCounter (yesOrNo) {
-    if (yesOrNo) {
+function ubc() { 
+    bookCount = 0;
+    read = 0;
+    unread = 0;
+    bookCountDisplay.innerText = "0";
+    readCountDisplay.innerText = "0";
+    unreadCountDisplay.innerText = "0";
 
-        console.log("true");
-        read++;
-        if(unread > 0) {
-            unread--;
+    for (let i = 0; i < myLibrary.length; i++) {
+        console.log("running " + i + " t or f " + myLibrary[i].haveRead );
+        //calc read/unread
+        bookCount++;
+        if(myLibrary[i].haveRead) {
+            read++;
+        } else {
+            unread++;
         }
-        readCountDisplay.innerText = read;
-        unreadCountDisplay.innerText = unread;
-    } else {
-        console.log("false");
-        unread++;
-        if(read > 0) {
-            read--;
-        }
-        readCountDisplay.innerText = read;
-        unreadCountDisplay.innerText = unread;
     }
+    bookCountDisplay.innerText = `${bookCount}`;
+    readCountDisplay.innerText = `${read}`;
+    unreadCountDisplay.innerText = `${unread}`;
 }
-
-
-function updateBookCounterNew (yesOrNo) {
-    if (yesOrNo) {
-        read++;
-        // if(unread > 0) {
-        //     unread--;
-        // }
-        readCountDisplay.innerText = read;
-        unreadCountDisplay.innerText = unread;
-    } else {
-        unread++;
-        // if(read > 0) {
-        //     read--;
-        // }
-        readCountDisplay.innerText = read;
-        unreadCountDisplay.innerText = unread;
-    }
-}
-
-// function updateBookCounter (yesOrNo) {
-    
-// for (let i = 0; i < myLibrary.length; i++) {
-//     // addBookToList(myLibrary[i].title, myLibrary[i].author, myLibrary[i].pages, myLibrary[i].haveRead, i);
-//     // myLibrary[i].idNum = i;
-//     bookCount++;
-//     readCountDisplay.innerText = read;
-//     unreadCountDisplay.innerText = unread;
-// }  
-// }
+//adjusted
+// change
